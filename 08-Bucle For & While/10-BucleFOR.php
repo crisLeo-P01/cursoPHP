84d38207b5374fad5dbeba2b150e8f6f488a80f0
crisLeo <cristianleopaiva@gmail.com>
2021-09-02 19:48:31 -0300

    Bucle For & While

--- 08-Bucle For & While/10-BucleFOR.php
+++ 08-Bucle For & While/10-BucleFOR.php
@@ -1,24 +1,30 @@
 <?php
 $num = 0;
 
-for ($i = 0; $i <= 100; $i++) {
-    $num += $i;
-    echo "$num <br>";
+for ($i = 0; $i <= 100; $i ++) {
+    $num += $i; // Esto es igual a => $num = $num + $i;
 }
+    echo "El resultado es: $num <br>";
 
 echo "El resultado es $num <br>";
 
-for ($z=0; $z<=20; $z+=2) {
-  echo "Bucle for <br>";
+for ($z = 0; $z <= 20; $z += 2) {
+  echo "Bucle for $z<br>";
   if ($z == 6) {
     echo "Programa interrumpido <br>";
     break;
   }
 }
 
+if (isset($_GET['numero'])) {
+    $numero = (int)$_GET['numero'];
+} else {
+    $numero = 1;
+}
+
 //Creando una tabla de multiplicar de 8
-for($f=0; $f<=10; $f++) {
-  echo "El resultado de 9 x $f es " . 9*$f . "<br>";
+for($f = 0; $f <= 10; $f ++) {
+  echo "El resultado es " . $numero * $f . "<br>";
 }
 
 echo "Tabla del 9, finalizada";
