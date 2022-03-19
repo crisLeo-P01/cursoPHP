7c5da42bc7e89088e9f765360b2827ed5e0148ff
Cristian Paiva <38576658+crisLeo-P01@users.noreply.github.com>
2022-03-19 02:07:12 -0300

    Update 11-1-Funciones.php

--- 09-Funciones/11-1-Funciones.php
+++ 09-Funciones/11-1-Funciones.php
@@ -2,36 +2,54 @@
 
 $nombre = "crisTian";
 
-echo(strtolower($nombre) . "<br>"); //Pone al string todo en minúsculas
-echo(strtoupper($nombre) . "<br>"); //Pone al string todo en mayúsculas
+echo( strtolower( $nombre ) . "<br>" ); // Pone al string todo en minúsculas
+echo( strtoupper( $nombre ) . "<br>" ); // Pone al string todo en mayúsculas
 
 //////////////////////////////////////////////////
 
-function suma($num1, $num2) {
-  echo "La suma entre $num1 y $num2 es de " . ($num1 + $num2) . "<br>";
+function suma( $num1, $num2 ) {
+  echo "La suma entre $num1 y $num2 es de " . ( $num1 + $num2 ) . "<br>";
 }
 
-suma(5, 7);
+suma( 5, 7 );
 
-function multiplicar($num3, $num4) {
+function multiplicar( $num3, $num4 ) {
   $resultado = $num3 * $num4;
   return $resultado;
 }
 
-echo (multiplicar(5, 9) . "<br>");
+echo (multiplicar( 5, 9 ) . "<br>");
 
 //////////////////////////////////////////////////
 
-function fraseMayus($frase, $conversion=true) {
-  $frase = strtolower($frase);
-  if($conversion == true) {
-    $resultado02 = ucwords($frase);
+function tablaMultiplicar($numMulti) {
+    echo "<h4>La tabla de multiplicar del $numMulti</h4>";
+    for( $i = 1; $i <= 10; $i++ ) {
+        $operacion_multi = $numMulti * $i;
+        echo "$numMulti x $i = $operacion_multi <br>";
+    }
+}
+tablaMultiplicar(9);
+
+//Otra forma es ingresando un número a través de la URL
+if(isset($_GET['numeroget'])) {
+    tablaMultiplicar($_GET['numeroget']);
+} else {
+    echo 'No hay tabla por multiplicar';
+}
+
+//////////////////////////////////////////////////
+
+function fraseMayus( $frase, $conversion = true ) {
+  $frase = strtolower( $frase );
+  if( $conversion == true) {
+    $resultado02 = ucwords( $frase ); // Convierte en mayúscula la primera letra del string
   } else {
-    $resultado02 = strtoupper($frase);
+    $resultado02 = strtoupper( $frase );
   }
 
   return $resultado02;
 }
 
-echo (fraseMayus("esto es una prueba"));
+echo ( fraseMayus( "esto es una prueba" ) );
 
