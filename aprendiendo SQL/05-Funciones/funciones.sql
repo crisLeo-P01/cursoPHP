1e108a1ee11886c1618eb5c898fe23769874b743
crisLeo-P01 <cristianleopaiva@gmail.com>
2022-08-29 01:57:12 -0300

    Aprendiendo SQL

--- aprendiendo SQL/05-Funciones/funciones.sql
+++ aprendiendo SQL/05-Funciones/funciones.sql
@@ -3,3 +3,9 @@ SELECT CEIL(7.89) AS 'Operaciones' FROM usuarios; # redondea para arriba
 SELECT FLOOR(7.89) AS 'Operaciones' FROM usuarios; # redondea para abajo
 SELECT ABS(-7.14) AS 'Operaciones' FROM usuarios; # convierte todo numero negativo en positivo
 SELECT ROUND(3.51) AS 'Operaciones' FROM usuarios; # devuelve el valor de X redondeado al entero más cercano a él.
+
+# FUNCIONES DE TEXTOS #
+SELECT UPPER(nombre) FROM usuarios;
+SELECT LOWER(nombre) FROM usuarios;
+SELECT LENGHT(nombre) FROM usuarios;
+SELECT CONCAT(id, ' ', nombre) FROM usuarios;
