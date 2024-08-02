c27aed9ad5a84603932c515778970ee4081f7e18
Cristian Paiva <38576658+crisLeo-P01@users.noreply.github.com>
2024-08-02 08:16:10 -0300

    Update - refactorizando código mas legible

--- 18-SubidaArchivo/vistaImagenes.php
+++ 18-SubidaArchivo/vistaImagenes.php
@@ -2,13 +2,25 @@
 
 $dir = './images/';
 
-if( is_dir( $dir ) ) {
-  if( $gestor = opendir( $dir ) ) {
-    while( ( $image = readdir( $gestor ) ) !== false ) {
-      if( $image != '.' && $image != '..' ) {
-        echo "<img src='images/$image' width=250px /><br>";
-      }
+$dir = './images/';
+
+if (is_dir($dir)) {
+  $images = scandir($dir);
+
+  foreach ($images as $image) {
+    if ($image != '.' && $image != '..') {
+      echo "<img src='images/$image' width='250px' /><br>";
     }
-    closedir( $gestor );
   }
 }
+
+// if( is_dir( $dir ) ) {
+//   if( $gestor = opendir( $dir ) ) {
+//     while( ( $image = readdir( $gestor ) ) !== false ) {
+//       if( $image != '.' && $image != '..' ) {
+//         echo "<img src='images/$image' width=250px /><br>";
+//       }
+//     }
+//     closedir( $gestor );
+//   }
+// }
